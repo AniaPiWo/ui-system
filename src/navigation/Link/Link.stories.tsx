f7e4b527947ab6f1b37bbfab4cbdbc0aa540fe03
AniaPiWo <snowflake@wp.pl>
2023-02-15 16:09:28 +0100

    link ver 2
    please, let it be legit this time, <praying> :D

---
+++ src/navigation/Link/Link.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, Story } from "@storybook/react";
+import * as React from "react";
+import { Link as MyLink } from "./Link";
+
+export default {
+  component: MyLink,
+} as Meta;
+export const Link: Story = (args) => (
+  <MyLink
+    id={"link-1"}
+    to={"www.google.com"}
+    target={"_blank"}
+    onClick={() => {
+      console.log("Button clicked!");
+    }}
+    {...args}
+  >
+    This is link
+  </MyLink>
+);
+
+Link.args = {
+  id: "link-1",
+  to: "www.google.com",
+  disabled: false,
+};
