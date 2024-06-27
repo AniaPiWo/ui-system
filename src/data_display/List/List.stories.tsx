29b0d14b83725a3faef7b457c8657bcfb28afd32
AniaPiWo <snowflake@wp.pl>
2024-06-27 14:13:39 +0200

    feat: list

---
+++ src/data_display/List/List.stories.tsx
@@ -0,0 +1,79 @@
+import React, { Fragment } from "react";
+import { Meta, Story } from "@storybook/react";
+import { List, ListItem, ListHeader, ListText, ListProps } from "./List";
+import Avatar from "../Avatar/Avatar";
+import { Switch } from "../../inputs/Switch/Switch";
+import { Checkbox } from "../../inputs/Checkbox/Checkbox";
+
+export default {
+  title: "DataDisplay/List",
+  component: List,
+} as Meta;
+
+export const TextStory: Story<ListProps> = (args) => (
+  <List>
+    <ListItem>
+      <ListHeader>List item</ListHeader>
+      <ListText>
+        Supporting line text lorem ipsum dolor sit amet, consectetur Lorem ipsum
+      </ListText>
+    </ListItem>
+  </List>
+);
+
+export const HeaderOnlyStory: Story<ListProps> = (args) => (
+  <List>
+    <ListItem>
+      <ListHeader>List item</ListHeader>
+    </ListItem>
+  </List>
+);
+
+export const AvatarStory: Story<ListProps> = (args) => (
+  <List>
+    <ListItem>
+      <Avatar size="big" alt="Avatar" />
+    </ListItem>
+    <ListItem>
+      <ListHeader>List item</ListHeader>
+      <ListText>
+        Supporting line text lorem ipsum dolor sit amet, consectetur
+      </ListText>
+    </ListItem>
+  </List>
+);
+
+export const SwitchStory: Story<ListProps> = (args) => (
+  <List>
+    <ListItem>
+      <Switch
+        id="switch-1"
+        checked={false}
+        onChange={() => {
+          console.log("switched");
+        }}
+      />
+    </ListItem>
+    <ListItem>
+      <ListHeader>List item</ListHeader>
+    </ListItem>
+  </List>
+);
+
+export const CheckboxStory: Story<ListProps> = (args) => (
+  <List>
+    <ListItem>
+      <ListHeader>List item</ListHeader>
+    </ListItem>
+    <ListItem>
+      <Checkbox
+        id="checkbox-1"
+        checked={true}
+        label="Checkbox item"
+        onChange={() => {
+          console.log("checked");
+        }}
+      />
+    </ListItem>
+  </List>
+);
---
+++ src/data_display/List/List.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./List.module.css";
+
+export interface ListProps {
+  children: React.ReactNode;
+}
+
+export const List: React.FC<ListProps> = ({ children }) => {
+  return <ul className={styles.list}>{children}</ul>;
+};
+
+export const ListItem: React.FC<ListProps> = ({ children }) => {
+  return <li className={styles.listItem}>{children}</li>;
+};
+
+export const ListHeader: React.FC<ListProps> = ({ children }) => {
+  return <li className={styles.listHeader}>{children}</li>;
+};
+
+export const ListText: React.FC<ListProps> = ({ children }) => {
+  return <li className={styles.listText}>{children}</li>;
+};
